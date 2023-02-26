1b69cdbc268b8a181025f9ddb8fef79d1866f57a
Kristian <kpskola1002@gmail.com>
2023-02-26 09:58:41 +0100

    Fix example file

--- app/index.php
+++ app/index.php
@@ -12,7 +12,7 @@
     <?php
     
     try {
-        $conn = new PDO("mysql:host=mysql;dbname=school", "internal", "password");
+        $conn = new PDO("mysql:host=mysql;dbname=school", "internal", "secret");
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
